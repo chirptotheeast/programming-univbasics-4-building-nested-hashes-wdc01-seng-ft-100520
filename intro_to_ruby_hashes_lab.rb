8950423a0cbfcf59e6e75b15f33f313f88797cbb
Alyssa Easterly <alyssa.easterly@yahoo.com>
2020-09-17 21:47:06 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -12,7 +12,10 @@ def monopoly_with_second_tier
   # complexity.
    monopoly = {:railroads  => {
       :pieces => 4,
-      :rent_in_dollars => {},
+      :rent_in_dollars => {
+        :one_piece_owned => 25,
+        :
+      },
       :names => {}
     }
    }
